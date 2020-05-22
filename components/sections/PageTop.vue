8fd931e46c759c18775d3cfe86a45c513f3b4c36
Yuta Toyokawa <kppg42@gmail.com>
2020-05-22 23:10:19 +0900

    top imageをスライドに変更

--- components/sections/PageTop.vue
+++ components/sections/PageTop.vue
@@ -1,23 +1,78 @@
 <template lang="pug">
-  #pageTop.page-top
-    .title-area
-      .title Learn de Leon
-      .subtitle 「やる」だけじゃない、思考力を育てる。
+  #pageTop
+    hooper(:settings="hooperSettings")
+      slide
+        .page-top(:style="{ backgroundImage: `url('${topImage1}')`}")
+          .title-area
+            .title-lg Learn de Leon
+            .subtitle 「やる」だけじゃない、思考力を育てる
+      slide
+        .page-top(:style="{ backgroundImage: `url('${topImage2}')`}")
+          .title-area
+            .title-sm 教育を「エンタメ」に
+            .subtitle Education to Entertainment
+      hooper-pagination(slot="hooper-addons")
 </template>
+
+<script>
+import { Hooper, Slide, Pagination as HooperPagination } from "hooper";
+import "hooper/dist/hooper.css";
+import topImage1 from "~/assets/images/top_image.jpg";
+import topImage2 from "~/assets/images/top_image2.jpg";
+
+export default {
+  components: {
+    Hooper,
+    Slide,
+    HooperPagination
+  },
+  data() {
+    return {
+      hooperSettings: {
+        itemsToShow: 1,
+        infiniteScroll: true,
+        autoPlay: true,
+        playSpeed: 7000,
+        transition: 750,
+        hoverPause: false,
+        mouseDrag: false,
+        touchDrag: false,
+        shortDrag: false,
+        wheelControl: false
+      },
+      topImage1,
+      topImage2
+    };
+  }
+};
+</script>
+
+<style lang="sass">
+.hooper
+  height: inherit
+  .hooper-pagination
+    .hooper-indicators
+      .hooper-indicator
+        width: 10px
+        height: 10px
+        margin: 0 4px
+        border-radius: 50%
+        &.is-active, &:hover
+          background-color: #63AEE5
+</style>
 
 <style lang="sass" scoped>
 .page-top
   position: relative
   width: 100%
   height: 100vh
-  background-image: url("~assets/images/top_image.jpg")
   background-size: cover
   background-position: center center
   &:after
     content: ''
     width: 100%
     height: 100vh
-    background-color: rgba(0, 0, 0, 0.15)
+    background-color: rgba(0, 0, 0, 0.2)
     position: absolute
     top: 0
     left: 0
@@ -33,16 +88,24 @@
     margin: auto
     z-index: 100
     @include media-breakpoint-up(sm)
-      .title
+      .title-lg
         font-size: 60px
+        font-weight: bold
+        letter-spacing: 5px
+      .title-sm
+        font-size: 54px
         font-weight: bold
         letter-spacing: 5px
       .subtitle
         font-size: 24px
         letter-spacing: 3px
     @include media-breakpoint-down(xs)
-      .title
-        font-size: 42px
+      .title-lg
+        font-size: 36px
+        font-weight: bold
+        letter-spacing: 5px
+      .title-sm
+        font-size: 30px
         font-weight: bold
         letter-spacing: 5px
       .subtitle
