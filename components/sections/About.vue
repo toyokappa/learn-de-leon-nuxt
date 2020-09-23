504dcbcdf6c58b84ff1704db32f249feb0bc6349
Yuta Toyokawa <kppg42@gmail.com>
2020-09-23 18:21:05 +0900

    概要をContentfulから引けるように修正

--- components/sections/About.vue
+++ components/sections/About.vue
@@ -1,62 +1,31 @@
 <template lang="pug">
-  .about
-    .container
-      .row.about-section
-        .col-lg-6.order-lg-2.mb-sx-3
-          img(v-lazy="tampopo")
-        .col-lg-6.order-lg-1
-          .description
-            | 「ランデレオン」は、群馬県邑楽郡を中心に展開している家庭教師事業団体です。
-            br
-            br
-            | 「ランデレオン」という名前はは英語の「Learn（学ぶ）」とスペイン語の「ディエンテ・デ・レオン（たんぽぽ）」の2つの言葉を組み合わせて作りました。
-            br
-            br
-            | 「たんぽぽ」はどんな状況にも適用でき、自らの力で種を作り、風にのってあらゆる場所に行くことができます。とても強い花です。
-            br
-            br
-            | 学びの世界でたんぽぽの様に、どんな状況でもて適用でき、自らが考えて行動をし、あらゆる分野で活躍をするための基礎力を育てたい、そういう想いを込めて「ランデレオン」という名をつけました。
-            br
-            br
-            | 実際の授業でも、お子さんの自主性を育てることに重きをおいています。最終的には我々のもとを巣立ち、自ら学習を進めてもらえるような土壌づくりを目標としています。
-      .row
-        .col-lg-6.mb-sx-3
-          img(v-lazy="learn")
-        .col-lg-6
-          .description
-            | 私達は、「知る」ことで「成長する」、「成長する」ことで「楽しくなる」、「楽しくなる」ことで「また知りたくなる」、このサイクルを大事にしています。
-            br
-            br
-            | このサイクルによって生み出されるのは、「学び」だけではなありません。「何かをしたい」という感情を引き出すことができます。
-            br
-            br
-            | この「何かをしたい」という感情こそが、行動や学びの原動力となるのです。やらされて勉強をするのではなく、自らの欲求のもと学ぶのです。
-            br
-            br
-            | 私達は、当然ながら学習指導要領に基づいた勉強の仕方を教えます。しかし、学校の予備として教えるつもりはありません。
-            br
-            br
-            | 子どもたちが楽しく、柔軟に、そして今後の社会を生き抜くための考える力を養います。そういった個別のカリキュラムも用意しています。
-            br
-            br
-            | 私達は、一人として同じ教え方をすることはありません。短期集中で詰め込み型の子もいれば、大器晩成であとから実績がついてくる子もいます。計算が得意な子もいれば、暗記が得意な子もいます。
-            br
-            br
-            | 100人いれば100通りの教え方が必要であると考えです。そういった考えで一人ひとりに合った学習サポートを行うことをお約束します。
+.about
+  .container
+    .row.about-section
+      .col-lg-6.order-lg-2.mb-sx-3
+        img(v-lazy="about.fields.section1_image.fields.file.url")
+      .col-lg-6.order-lg-1
+        .description(v-html="parseDescription(about.fields.section1_text)")
+    .row
+      .col-lg-6.mb-sx-3
+        img(v-lazy="about.fields.section2_image.fields.file.url")
+      .col-lg-6
+        .description(v-html="parseDescription(about.fields.section2_text)")
 </template>
 
 <script>
-import tampopo from "@/assets/images/tampopo.jpg"
-import learn from "@/assets/images/learn.svg"
-
 export default {
-  data() {
-    return {
-      tampopo,
-      learn
-    }
-  }
-}
+  props: {
+    about: {
+      type: Object,
+    },
+  },
+  methods: {
+    parseDescription(text) {
+      return text.replace(/\n/g, "<br>");
+    },
+  },
+};
 </script>
 
 <style lang="sass" scoped>
--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,47 @@
 <template lang="pug">
-  #wrapper
-    g-header
-    section.section
-      s-page-top
-    section.section
-      p-section-header#about(
-        title="ランデレオンとは？"
-        subtitle="About Learn de Leon"
-        description="親御さんに知ってほしいこと"
-      )
-      s-about
-    section.section
-      p-section-header#profile(
-        title="運営者・講師"
-        subtitle="Member's Profile"
-        description="ともに歩む仲間たち"
-      )
-      s-member(:memberList="memberList")
-    section.section
-      p-section-header#blog(
-        title="最新情報"
-        subtitle="Recent Blog Posts"
-        description="最新の動向をコンテンツとしてお届け"
-      )
-      s-blog(:blogPosts="blogPosts")
-      p-blog-link-button
-    section.section
-      p-section-header#flow(
-        title="授業開始までの流れ"
-        subtitle="How To Start"
-        description="ご利用前にご安心いただくために"
-      )
-      s-flow
-    section.section.section-grey
-      p-section-header#contact(
-        title="お問い合わせ"
-        subtitle="Get In Touch"
-        description="些細なことでも お気軽に ご相談ください"
-        titleColor="#63AEE5"
-        subtitleColor="black"
-      )
-      s-contact
-    g-footer
+#wrapper
+  g-header
+  section.section
+    s-page-top
+  section.section
+    p-section-header#about(
+      title="ランデレオンとは？",
+      subtitle="About Learn de Leon",
+      description="親御さんに知ってほしいこと"
+    )
+    s-about(:about="aboutItem")
+  section.section
+    p-section-header#profile(
+      title="運営者・講師",
+      subtitle="Member's Profile",
+      description="ともに歩む仲間たち"
+    )
+    s-member(:memberList="memberList")
+  section.section
+    p-section-header#blog(
+      title="最新情報",
+      subtitle="Recent Blog Posts",
+      description="最新の動向をコンテンツとしてお届け"
+    )
+    s-blog(:blogPosts="blogPosts")
+    p-blog-link-button
+  section.section
+    p-section-header#flow(
+      title="授業開始までの流れ",
+      subtitle="How To Start",
+      description="ご利用前にご安心いただくために"
+    )
+    s-flow
+  section.section.section-grey
+    p-section-header#contact(
+      title="お問い合わせ",
+      subtitle="Get In Touch",
+      description="些細なことでも お気軽に ご相談ください",
+      titleColor="#63AEE5",
+      subtitleColor="black"
+    )
+    s-contact
+  g-footer
 </template>
 
 <script>
@@ -67,25 +67,33 @@ export default {
     SMember,
     SBlog,
     SFlow,
-    SContact
+    SContact,
   },
   async asyncData({ app }) {
     const blogRes = await app.$ctfClient.getEntries({
       content_type: "blogPosts",
       order: "-sys.createdAt",
-      limit: 3
+      limit: 3,
     });
     const blogPosts = blogRes.items;
 
     const memberRes = await app.$ctfClient.getEntries({
       content_type: "member",
-      order: "fields.position"
+      order: "fields.position",
     });
     const memberList = memberRes.items;
 
+    const aboutRes = await app.$ctfClient.getEntries({
+      content_type: "about",
+      order: "sys.createdAt",
+      limit: 1,
+    });
+    const aboutItem = aboutRes.items[0];
+
     return {
       blogPosts,
-      memberList
+      memberList,
+      aboutItem,
     };
   },
   head() {
@@ -104,10 +112,10 @@ export default {
         { property: "og:title", content: pageTitle },
         { property: "og:type", content: "website" },
         { property: "og:image", content: imageUrl },
-        { property: "og:description", content: description }
-      ]
+        { property: "og:description", content: description },
+      ],
     };
-  }
+  },
 };
 </script>
 
